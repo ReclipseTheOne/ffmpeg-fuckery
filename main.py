e8606d96b9792b8ca76bb48ee1319c09c2a0f2a3
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-01-04 22:11:46 +0200

    Initial Commit or smth

---
+++ main.py
@@ -0,0 +1,116 @@
+import pathlib
+import ffmpeg
+
+from math import floor
+from pprint import pp
+from rites import rituals
+
+p = rituals.Misc
+
+# Search for all the files in the current directory
+current_directory = pathlib.Path(__file__).parent / "input"
+
+paths = [path for path in current_directory.glob("*")]
+
+# Create a dictionary with the index as keys
+dict_paths = {index: path for index, path in enumerate(paths)}
+
+dict_conversions = {
+    1: "MP3",
+    2: "MP4",
+    3: "GIF",
+    4: "Exit"
+}
+
+
+def getSecondsFromStream(file_choice: pathlib.WindowsPath, stream: int) -> int:
+    duration = ffmpeg.probe(file_choice)['streams'][stream]['tags']['DURATION']
+    (h, m, s) = duration.split(":")
+    return int(h) * 3600 + int(m) * 60 + int(floor(float(s)))
+
+
+def getInput(prompt: str, verifier) -> int:
+    while True:
+        try:
+            value = int(input(prompt))
+            if verifier(value):
+                return value
+            else:
+                p.print_error("Invalid input. Please try again.")
+        except ValueError:
+            p.print_error("Invalid input. Please enter a number.")
+
+
+def getExtension(path: pathlib.WindowsPath) -> str:
+    return path.suffix
+
+
+def main():
+    p.print_info("FFMPEG FUCKERY")
+
+    for index, path in dict_paths.items():
+        print(f"{index}. {path}")
+
+    file_choice = dict_paths[getInput(">", lambda x: x in dict_paths.keys())]
+    ext = file_choice.suffix
+
+    p.print_info(f"Selected file: {file_choice}")
+    print(f"1. Conversion\n2. Remove Audio\n3. Extract Audio\n4. Trim\n5. Exit")
+
+    process_choice = getInput(">", lambda x: x in {1, 2, 3, 4, 5})
+    if process_choice == 5:
+        return
+
+    if process_choice == 1:
+        p.print_info(f"Conversing {file_choice} to:")
+
+        for index, conversion in dict_conversions.items():
+            print(f"{index}. {conversion}")
+
+        conversion_choice = getInput(">", lambda x: dict_conversions.keys().__contains__(x))
+        if dict_conversions[conversion_choice] == "Exit":
+            return
+
+        p.print_info(f"Converting {ext} to {dict_conversions[conversion_choice]}")
+        output_path = pathlib.Path(__file__).parent / "output" / f"{file_choice.stem}.{dict_conversions[conversion_choice].lower()}"
+        ffmpeg.input(file_choice).output(str(output_path)).run()
+
+    elif process_choice == 2:
+        output_path = pathlib.Path(__file__).parent / "output" / f"{file_choice.stem}_noaudio{ext}"
+        ffmpeg.input(file_choice).output(str(output_path), **{"c:a": "copy", "c:v": "copy", "an": None}).run()
+
+    elif process_choice == 3:
+        output_path = pathlib.Path(__file__).parent / "output" / f"{file_choice.stem}_audio.mp3"
+        ffmpeg.input(file_choice).output(str(output_path), **{"vn": None}).run()
+
+    elif process_choice == 4:
+        s = getSecondsFromStream(file_choice, 0)
+        duration = s
+
+        m = s // 60
+        h = m // 60
+        if (duration >= 3600):
+            m = m % 60
+            s = s % 60
+            p.print_info(f"Selected file has a play time of: {duration} seconds / {h}:{m}:{s}")
+        elif (duration >= 60):
+            s = s % 60
+            p.print_info(f"Selected file has a play time of: {duration} seconds / {m}:{s}")
+        elif (duration == 1):
+            p.print_info(f"Selected file has a play time of: {duration} second")
+        else:
+            p.print_info(f"Selected file has a play time of: {duration} seconds")
+
+        start_time = getInput("Start time (in seconds): ", lambda x: {
+            duration > x >= 0
+        })
+        end_time = getInput("End time (in seconds): ", lambda x: {
+            duration >= x > start_time
+        })
+
+        output_path = pathlib.Path(__file__).parent / "output" / f"{file_choice.stem}_trimmed{ext}"
+        ffmpeg.input(file_choice, ss=start_time, to=end_time).output(str(output_path)).run()
+
+
+if __name__ == "__main__":
+    main()
